d57c7194dc07a4c6a3c680d8a3c420052abe1c01
JedrekSt <stempinjk@gmail.com>
2025-05-01 12:32:58 +0200

    updated ssqw

--- OldQuantumWalkModule/plotting.py
+++ OldQuantumWalkModule/plotting.py
@@ -26,8 +26,8 @@ def berry_bloch_phase(th1,th2,ns,th_1,th_2,data_ans):
     ]
 
     origin = [0, 0, 0]  
-    for v in vectors:
-        ax1.quiver(*origin, *v, color='g', linewidth=1,  normalize=True)
+    """for v in vectors:
+        ax1.quiver(*origin, *v, color='black', linewidth=1,  normalize=True)"""
 
     for i in range(ns[0].shape[1]):
         ax1.quiver(*origin, *ns[1][:,i], color='b', linewidth=1,alpha = 0.3)
@@ -37,20 +37,20 @@ def berry_bloch_phase(th1,th2,ns,th_1,th_2,data_ans):
     ax1.set_xlabel('X')
     ax1.set_ylabel('Y')
     ax1.set_zlabel('Z')
-    ax1.set_title('Bloch vectors for k in BZ')
+    #ax1.set_title('Bloch vectors for k in BZ')
     ax1.set_box_aspect([1, 1, 1])
-    ax1.grid(False)
+    ax1.grid(True)
 
     ax1.set_xticklabels([])
     ax1.set_yticklabels([])
     ax1.set_zticklabels([])
 
-    cax = ax2.contourf(th_1,th_2,data_ans/np.pi,cmap = "viridis")
+    cax = ax2.contourf(th_1,th_2,data_ans/np.pi,cmap = "viridis", levels = 1)
     fig.colorbar(cax)
-    ax2.set_xlabel(r"$\theta_{2}$")
-    ax2.set_ylabel(r"$\theta_{1}$")
-    ax2.set_title("winding number for single step QW")
-    ax2.scatter([th1],[th2],color="blue", alpha = 1)
+    ax2.set_xlabel(r"$\theta_{-}$")
+    ax2.set_ylabel(r"$\theta_{+}$")
+    #ax2.set_title("winding number for single step QW")
+    ax2.scatter([th1],[th2],color="red", alpha = 0.7)
 
 
-    return ax1,ax2
+    return ax1,ax2, fig
